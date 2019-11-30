264b2013d5c7ffde6baef1716c98f05f4242498f
tameuszz <mat.dusik@gmail.com>
2019-11-30 23:35:56 +0100

    added plugins

--- common/src/main/java/com/calculator/Main.java
+++ common/src/main/java/com/calculator/Main.java
@@ -1,9 +1,44 @@
 package com.calculator;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class Main {
     public static void main( String[] args )
     {
+        File pluginsDir = new File("C:\\Users\\mateu\\Desktop\\TO_lab\\plugins");
+        File plugins = new File("C:\\Users\\mateu\\Desktop\\TO_lab\\plugins\\com\\calculator");
+        String[] files = plugins.list();
+
         ICalc calc = new Calc();
-        calc.solve("(3+9)/4");
+
+        if (files.length != 0) {
+            try {
+                URL loadPath = pluginsDir.toURI().toURL();
+                URL[] classUrl = new URL[]{loadPath};
+                ClassLoader cl = new URLClassLoader(classUrl);
+                System.out.print("Załadowane pluginy: ");
+                for (String file : files) {
+                    System.out.print(file + ", ");
+                    Class loadedClass = cl.loadClass("com.calculator." + file.replace(".class", ""));
+                    Object obj = loadedClass.newInstance();
+                    IPlugins plugin = (IPlugins) obj;
+                    calc.setAllOperators(plugin.getOperator());
+                    calc.setOperatorMap(plugin.getOperator(), plugin.getOperatorWeight());
+                    calc.setOperationMap(plugin.getOperator(), plugin);
+                }
+
+                calc.solve("((4 + 5) / 3)^2");
+
+            } catch (MalformedURLException | ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- interface/src/main/java/com/calculator/ICalc.java
+++ interface/src/main/java/com/calculator/ICalc.java
@@ -2,4 +2,8 @@ package com.calculator;
 
 public interface ICalc {
     double solve(String expression);
+    void setOperatorMap(String sign, Integer weight);
+    void setAllOperators(String sign);
+    public void setOperationMap(String sign , Object operation);
+
 }
